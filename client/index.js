349d535749bc100bad51ab67256a27bc2ebae273
Frederik Ring <frederik.ring@gmail.com>
2022-05-05 14:03:19 +0200

    Scaffold client interface

--- client/index.js
+++ client/index.js
@@ -1,1 +1,37 @@
-console.log('Hello world')
+class Client {
+  constructor (options = {}) {
+    this.proxy = new EmbeddedProxy(options.url)
+  }
+
+  acquire (...scopes) {
+    return this.proxy.send('ACQUIRE', ...scopes)
+  }
+
+  query (...scopes) {
+    return this.proxy.send('QUERY', ...scopes)
+  }
+
+  revoke (...scopes) {
+    return this.proxy.send('REVOKE', ...scopes)
+  }
+}
+
+class EmbeddedProxy {
+  constructor (url) {
+    this.iframe = this.injectIframe(url)
+
+    this.targetOrigin = new window.URL(url).origin
+  }
+
+  injectIframe (url) {
+    return Promise.resolve(window)
+  }
+
+  send (type, ...scopes) {
+    return this.iframe.then(el => {
+      el.postMessage({ type, scopes }, this.targetOrigin)
+    })
+  }
+}
+
+module.exports = Client
