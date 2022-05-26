e00d9d7aae647a4f107434e25c5da61c00b38ce2
Frederik Ring <frederik.ring@gmail.com>
2022-05-26 11:02:53 +0200

    Add AutoTLS feature

--- cmd/consent/main.go
+++ cmd/consent/main.go
@@ -12,10 +12,13 @@ import (
 	"time"
 
 	consent "github.com/offen/consent"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	port := flag.Int("port", 8000, "The port to bind to")
+	domain := flag.String("domain", "", "The domain used to serve the application via SSL")
+	certs := flag.String("certs", "/var/www/.cache", "The directory to use for caching SSL certificates")
 	flag.Parse()
 
 	logger := log.New(os.Stderr, "", log.Ldate)
@@ -30,12 +33,28 @@ func main() {
 		Addr:    fmt.Sprintf(":%d", *port),
 	}
 	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Fatalf("cmd: error starting server: %s", err.Error())
+		if *domain != "" {
+			m := autocert.Manager{
+				Prompt:     autocert.AcceptTOS,
+				HostPolicy: autocert.HostWhitelist(*domain),
+				Cache:      autocert.DirCache(*certs),
+			}
+			go http.ListenAndServe(":http", m.HTTPHandler(nil))
+			if err := http.Serve(m.Listener(), srv.Handler); err != nil && err != http.ErrServerClosed {
+				logger.Fatalf("cmd: error binding server to network: %s", err.Error())
+			}
+		} else {
+			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				logger.Fatalf("cmd: error starting server: %s", err.Error())
+			}
 		}
 	}()
 
-	logger.Printf("Server now listening on port %d", *port)
+	if *domain != "" {
+		logger.Printf("Server is using AutoTLS and is now listening on ports 80 and 443")
+	} else {
+		logger.Printf("Server now listening on port %d", *port)
+	}
 
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.18
 
 require (
 	github.com/evanw/esbuild v0.14.39 // indirect
-	golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/net v0.0.0-20220524220425-1d687d428aca // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
